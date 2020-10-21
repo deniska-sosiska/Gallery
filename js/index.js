b53c5b7473582bb57d3d730fbfd24efb3eb3244b
deniska-sosiska <denchikarabik@gmail.com>
2020-10-21 17:50:11 +0300

    jkj

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
 // import {model} from 'model.js'
 class ImageBlock {
-  constructor(name, link, options = '') {
+  constructor(name, link, info = {}) {
     this.name = name
     this.link = link
-    this.options = options
-  }
+    this.info = info
+  } //format, size, weight, mimetype, date, description
 }
 
 const model = [
-   new ImageBlock('Розы', 'Розы.jpg'),
+   new ImageBlock('Розы', 'Розы.jpg'
+   // {
+     // format: 'Портрет',
+     // size: ''
+     // weight:
+     // mimetype:
+     // date:
+     // description:
+   // }
+ ),
    new ImageBlock('Манхеттен', 'Манхеттен.jpg'),
    new ImageBlock('Лунная ночь', 'Лунная ночь.jpg'),
    new ImageBlock('Натюрморт с устрицами', 'Натюрморт с устрицами.jpg'),
@@ -21,16 +30,15 @@ const model = [
    // new ImageBlock('У пруда', 'У пруда.jpg')
 ]
 //=============================================================
+let $clean = document.querySelector('#clean')
+let $content = document.querySelector('#content_block')
 let $newImgForm = document.querySelector('#newImgForm')
 let $chgImgForm = document.querySelector('#chgImgForm')
-let $content = document.querySelector('#content_block')
 //======================IndexedDB==============================
-
-if (!window.indexedDB){
-  console.error('Ваш браузер не поддерживает indexedDB');
-}
-
 let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB
+
+if (!window.indexedDB)
+  console.error('Ваш браузер не поддерживает indexedDB');
 let request = indexedDB.open("gallery", 1)
 let db
 
@@ -46,31 +54,44 @@ request.onsuccess = function(event) {
   updateDisplay(db)
 }
 //===================================================================
-    //Добавление картины по пути. доделать
-const formAddImg = (form) => {
-  let str = form.file.value
-  let newStr = str.slice(12)
-  // console.log(str)
-  let item = {name: form.nameFile.value, link: newStr}
 
-  addImages(db, item)
-  form.style.display = 'none'
-}
-
-const newImagesForm = () => {
+const showAddForm = () => {
   $chgImgForm.style.display = 'none'
   $newImgForm.style.display = 'inline-block'
 }
+const showChangeImages = () => {
+  $newImgForm.style.display = 'none'
+  $chgImgForm.style.display = 'inline-block'
+}
 
 
-    //Добавление картин с массива model
-const imagesArr = () => {
-  model.forEach((item) => {
-    addImages(db, item)
-  });
+
+//Вызов ф. добавление картины по пути
+const addImgForm = () => {
+  let $files = $newImgForm.children[2].children[0].files
+  for(let i = 0; i < $files.length; i++){
+    let file = $files[i]
+    let fileName = file.name
+    var reader = new FileReader();
+    reader.onload = (function() {
+      let objImg = {link: reader.result, info: {name: fileName,  date: file.lastModifiedDate, size: file.size, mimetype: file.type}}
+      template(db, 'add', objImg)
+    })
+    reader.readAsDataURL(file);
+  }
+
+  $newImgForm.style.display = 'none';
+  $clean.style.display = 'inline-block'
 
 }
-    //Вывод в html
+   //Вызов ф. удаление картины по индексу
+const btnDelImages = () => {
+  template(db, 'delete')
+}   //Вызов ф. удаление картины по индексу
+const cleanStorageForm = () => {
+  template(db, 'clean')
+  $clean.style.display = 'none'
+}   //Вывод в html
 const outptHtml = (allImages) => {
   $content.innerHTML = '';
   allImages.forEach((item) => {
@@ -79,92 +100,96 @@ const outptHtml = (allImages) => {
     $content.insertAdjacentHTML('beforeend', html)
   })
 }
-const changeImages = () => {
-  $newImgForm.style.display = 'none'
-  $chgImgForm.style.display = 'inline-block'
-  // chgImages(db, id);
+          //=====================//
+
+const template = (db, option, item = {}) => {
+  let ts = db.transaction('images', 'readwrite')
+  let store = ts.objectStore('images')
+  if (option == 'add') {
+    let newImg = {link: item.link, info: item.info}
+    let req = store.add(newImg)
+  }
+  else if (option == 'delete') {
+    const ind = parseInt(prompt('"Удаление по индексу": Картину с каким индексом нужно удалить? '))
+    let req = store.getAll()
+      req.onsuccess = (event) => {
+        let key = req.result;
+        let deleteRequest = store.delete(ind)
+        console.log(req.result)
+      }
+  }
+  else if (option == 'clean') {
+      let req = store.clear();
+  }
+  ts.oncomplete = (event) => { updateDisplay(db) }
+  ts.onerror = function(event) {
+    console.log('error with transaction: ', option)
+  }
 }
-    //Удаление картины по индексу
-const delImages = () => {
-  deleteImages(db);
-}
-         //Добавление
-const addImages = (db, item) => {
-  let ts = db.transaction(['images'], 'readwrite')
+
+    //Функция добавления
+/*const addImages = (db, item) => {
+  let ts = db.transaction('images', 'readwrite')
   let store = ts.objectStore('images')
   let newImg = {name: item.name, link: item.link}
-  let added = store.add(newImg)
-  ts.oncomplete = () => {
-    console.log('images was add!')
-    updateDisplay(db)
+  let req = store.add(newImg)
+  ts.oncomplete = (event) => { updateDisplay(db) }
+  ts.onerror = function(event) {
+    console.log('error with transaction: add')
   }
-  ts.onerror = (event) => {
-    console.log('error adding images: ' + event.target.errorCode);
-  }
-}
-   //Просмотр данных
+}*/   //Функция просмотра данных
 const updateDisplay = (db) => {
-  let ts = db.transaction(['images'], 'readonly')
+  let ts = db.transaction('images', 'readonly')
   let store = ts.objectStore('images')
   let req = store.openCursor()
   let allImages = []
-  req.onsuccess = (event) => {
-    let cursor = event.target.result;
-    if (cursor != null) {
-      allImages.push(cursor.value);
-      cursor.continue()
-    } else {  outptHtml(allImages)  }
-  }
-  req.onerror = (event) => {
-    console.log('error in cursor request ' + event.target.errorCode)
-  }
-}
-    //Замена картины
-const chgImages = (db, id) => {
-  let ts = db.transaction(['images'], 'readwrite')
+    req.onsuccess = (event) => {
+      let cursor = event.target.result;
+      if (cursor != null) {
+        allImages.push(cursor.value);
+        cursor.continue()
+      } else {  outptHtml(allImages)  }
+    }
+    req.onerror = (event) => {
+      console.log('error in cursor request ' + event.target.errorCode)
+    }
+  ts.oncomplete = () => { console.log('updated') }
+  ts.onerror = (event) => { console.log('error with transaction: update') }
+}   //Функция замены картины
+/*const chgImages = (db, id) => {
+  let ts = db.transaction('images', 'readwrite')
   let store = ts.objectStore('images')
   let newImg = {name: item.name, link: item.link}
   let added = store.put(newImg)
-  ts.oncomplete = () => {
-    console.log('images was add!')
-    updateDisplay(db)
-  }
-  ts.onerror = (event) => {
-    console.log('error adding images: ' + event.target.errorCode);
-  }
-}
-    //Удаление по индексу
+  ts.oncomplete = () => { updateDisplay(db) }
+  ts.onerror = (event) => { console.log('error with transaction: change') }
+}   //Функция удаление по индексу
 const deleteImages = (db) => {
-  const ts = db.transaction(['images'], 'readwrite');
-  const store = ts.objectStore('images');
+  let ts = db.transaction('images', 'readwrite')
+  let store = ts.objectStore('images')
   const req = store.getAll()
-  const q = parseInt(prompt('"Удаление по индексу": Картину с каким индексом нужно удалить? '));
-  req.onsuccess = (event) => {
-    let key = req.result;
-    let deleteRequest = store.delete(q);
-    deleteRequest.onsuccess = (event) => {
-      console.log('Delete request successful')
-      updateDisplay(db)
-    };
-    deleteRequest.onerror = (event) => {
-      console.log('Ошибка удаления: ', event.target.errorCode)
+  const q = parseInt(prompt('"Удаление по индексу": Картину с каким индексом нужно удалить? '))
+    req.onsuccess = (event) => {
+      let key = req.result;
+      let deleteRequest = store.delete(q)
     }
-  }
-  ts.oncomplete = (event) => {
-    console.log('Удаление прошло успешно')
-    updateDisplay(db);
-  };
-  ts.onerror = function(event) {
-    console.log('error in cursor request ' + event.target.errorCode);
-  };
-}
-
+  ts.oncomplete = (event) => { updateDisplay(db) }
+  ts.onerror = function(event) { console.log('error with transaction: delete') }
+}   //Функция очистки хранилища
+const cleanStorage = (db) => {
+  let ts = db.transaction('images', 'readwrite')
+  let store = ts.objectStore('images')
+  let req = store.clear();
+  ts.oncomplete = (event) => { updateDisplay(db) }
+  ts.onerror = function(event) { console.log('error with transaction: clean') }
+}*/
+//===============================//
 //================================================================
 function newImage(item) {
   return `
   <div class="image">
-    <img src="image/${item.link}">
-    <p>${item.name}</p>
+    <img src="${item.link}">
+    <p>${item.info.name}</p>
   </div>
   `
 }
